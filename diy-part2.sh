3ac5fc4be3589ba34536bfc028159056dbdf79ca
imkenf <52485888+imkenf@users.noreply.github.com>
2022-09-27 10:39:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+wget -P packages/net/ddns-scripts/files/ https://github.com/imkenf/Actions-LEDE/blob/main/package/update_cloudflare_com_v4.sh
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
